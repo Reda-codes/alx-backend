8c40d39960e86ba0b197f8bb78c243dfb1b7db42
Reda <muhammadredabh@gmail.com>
2023-04-25 18:59:29 +0000

    fix

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Flask app """
-from flask import Flask, render_template
-from flask_babel import Babel
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
 from jinja2 import FileSystemLoader
 
 app = Flask(__name__)
