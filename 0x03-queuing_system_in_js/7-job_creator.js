b76a47bcd4022fe7316806b1fe7dc30bb64fab67
Reda <muhammadredabh@gmail.com>
2023-05-26 10:43:41 +0100

    9. Track progress and errors with Kue: Create the Job processor

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -51,25 +51,21 @@ const queue = kue.createQueue({ name: 'push_notification_code_2' });
 
 queue.setMaxListeners(0);
 
-for (const jobData of jobs) {
-  const job = queue.create('push_notification_code', jobData)
-    .save((err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(`Notification job created: ${job.id}`);
-      }
+for (const jobInfo of jobs) {
+  const job = queue.create('push_notification_code_2', jobInfo);
+
+  job
+    .on('enqueue', () => {
+      console.log(`Notification job created: ${job.id}`);
+    })
+    .on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+    })
+    .on('failed', (err) => {
+      console.log(`Notification job ${job.id} failed: ${err.toString()}`);
+    })
+    .on('progress', (progress, _data) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
     });
-
-  queue.on('complete', () => {
-    console.log('Notification job completed');
-  });
-
-  queue.on('progress', (progress) => {
-    console.log('Notification job', job.id, `${progress}% complete`);
-  })
-
-  queue.on('failed', () => {
-    console.log('Notification job failed');
-  });
+  job.save();
 }
